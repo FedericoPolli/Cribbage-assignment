600a891df7639a074e0aba17ae4dcd876242e043
Federico <polli.f@outlook.it>
2020-12-12 19:33:14 +0100

    Addressed issues 2 and 3: use card.equals() method to check whether cards are passed correctly, and refactored tests to address a single behaviour

--- src/test/java/dssc/assignment/cribbage/ParseCribbageHandTest.java
+++ src/test/java/dssc/assignment/cribbage/ParseCribbageHandTest.java
@@ -2,8 +2,7 @@ package dssc.assignment.cribbage;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ParseCribbageHandTest {
@@ -11,36 +10,31 @@ public class ParseCribbageHandTest {
     @Test
     void parseFiveOfHearts() {
         Card card = CardParser.parseCard("5H");
-        assertAll(
-                () -> assertEquals(Rank.FIVE, card.getRank()),
-                () -> assertEquals(Suite.HEARTS, card.getSuite()),
-                () -> assertEquals(5, card.valueFifteenTwos())
-        );
+        assert card.equals(new Card(Rank.FIVE, Suite.HEARTS));
     }
 
     @Test
-    void parseAceOfSpades() {
-        Card card = CardParser.parseCard("AS");
-        assertAll(
-                () -> assertEquals(Rank.ACE, card.getRank()),
-                () -> assertEquals(Suite.SPADES, card.getSuite()),
-                () -> assertEquals(1, card.valueFifteenTwos()),
-                () -> assertEquals(true, card.isAce())
-
-        );
+    void parseTwoOfSpades() {
+        Card card = CardParser.parseCard("2S");
+        assert card.equals(new Card(Rank.TWO, Suite.SPADES));
     }
 
     @Test
-    void parseJackOfDiamonds() {
-        Card card = CardParser.parseCard("jd");
-        assertAll(
-                () -> assertEquals(Rank.JACK, card.getRank()),
-                () -> assertEquals(Suite.DIAMONDS, card.getSuite()),
-                () -> assertEquals(10, card.valueFifteenTwos()),
-                () -> assertEquals(true, card.isJack())
-
-        );
+    void parseTenOfDiamondsInLowerCase() {
+        Card card = CardParser.parseCard("0d");
+        assert card.equals(new Card(Rank.TEN, Suite.DIAMONDS));
     }
 
+    @Test
+    void CheckIfAce() {
+        Card card = CardParser.parseCard("AH");
+        assertTrue(card.isAce());
+    }
+
+    @Test
+    void CheckIfNotJack() {
+        Card card = CardParser.parseCard("4C");
+        assertFalse(card.isJack());
+    }
 
 }
