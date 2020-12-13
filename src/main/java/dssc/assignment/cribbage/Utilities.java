4944ed951de5ed88898ce001e3093c54eb543bc7
Federico <polli.f@outlook.it>
2020-12-13 12:52:24 +0100

    Refactored code in Utilities

--- src/main/java/dssc/assignment/cribbage/Utilities.java
+++ src/main/java/dssc/assignment/cribbage/Utilities.java
@@ -1,10 +1,8 @@
 package dssc.assignment.cribbage;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Utilities {
     public static List<Integer> GenerateSubsetSums(List<Integer> set) {
@@ -22,7 +20,7 @@ public class Utilities {
         for (Integer element : set) {
             int size = Powerset.size();
             for (int i = 0; i < size; i++) {
-                TemporaryCopy = Powerset.get(i).stream().collect(Collectors.toList());
+                TemporaryCopy = new ArrayList<>(Powerset.get(i));
                 TemporaryCopy.add(element);
                 Powerset.add(TemporaryCopy);
             }
