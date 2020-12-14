77d4609951cbe5c84bc9708b11be9eb718540a22
Federico <polli.f@outlook.it>
2020-12-14 11:34:28 +0100

    Extracted method in GenerateSubsetSums

--- src/main/java/dssc/assignment/cribbage/Utilities.java
+++ src/main/java/dssc/assignment/cribbage/Utilities.java
@@ -1,21 +1,22 @@
 package dssc.assignment.cribbage;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Utilities {
     public static List<Integer> GenerateSubsetSums(List<Integer> set) {
         List<List<Integer>> PowerSet = GeneratePowerset(set);
-        /*
-        List<Integer> SubsetSums = new ArrayList<>();
-        for (List<Integer> Subset : PowerSet) {
-            SubsetSums.add(Subset.stream().mapToInt(Integer::intValue).sum());
-        }
-        return SubsetSums;
-        */
-        return PowerSet.stream().map( subset -> subset.stream().mapToInt(Integer::intValue).sum() ).collect(Collectors.toList());
+        return PowerSet.stream().map(SubsetToSubsetSum()).collect(Collectors.toList());
+    }
+
+    @NotNull
+    private static Function<List<Integer>, Integer> SubsetToSubsetSum() {
+        return subset -> subset.stream().mapToInt(Integer::intValue).sum();
     }
 
     public static List<List<Integer>> GeneratePowerset(List<Integer> set) {
